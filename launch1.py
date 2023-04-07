6d9fba4119b4e1306cd688b2803efd574464d3b6
Iyashi <126176394+Iyashinouta@users.noreply.github.com>
2023-04-08 05:53:49 +0700

    Update launch1.py

--- launch1.py
+++ launch1.py
@@ -346,16 +346,5 @@ def tests(test_dir):
     proc.kill()
     return exitcode
 
-
-def start():
-    print(f"Launching {'API server' if '--nowebui' in sys.argv else 'Web UI'} with arguments: {' '.join(sys.argv[1:])}")
-    import webui
-    if '--nowebui' in sys.argv:
-        webui.api_only()
-    else:
-        webui.webui()
-
-
 if __name__ == "__main__":
     prepare_environment()
-    start()
